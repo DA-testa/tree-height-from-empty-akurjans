fafecd4e100649fe193c22f8989991a2c6d60ff5
akurjans <118132555+akurjans@users.noreply.github.com>
2023-03-06 01:24:42 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,92 +1,58 @@
 import sys
 import threading
-import numpy as np
+
 
 def compute_height(n, parents):
-    # Write this function
-    x=np.array(parents)
-    
-    max_height = 0
-    max_heightK=0
-    node={}
-    # Your code here
-    k=0
-  
-    for i in range(n):
-        k=i
-        while True:
-            if x[i]==-1:
-                
-                k=i
-                break
-            else:
-                max_height+=1
-                
-                i=x[i]
-                
-                if i in node.keys():
-                    
-                    max_height=max_height+node.get(i)
-                    node[k]=max_height
-                    k=i
-                    break
-                else:
-                    node[k]=max_height
+    a= [0]*n
+    root= parents.index(-1)
 
-                 
-                
-                
-        if max_heightK<max_height:
-            max_heightK=max_height
-        max_height=0
-
+    def dfs(i) :
+        if a[i] != 0:
+            
+            return a[i]
         
-    
+        if parents[i] == -1:
+            
+            a[i] = 1
+        else:
+            a[i] = 1 + dfs(parents[i])
 
-    return max_heightK+1
+        return a[i]
+                
+    for i in range(n):
+        dfs(i)
 
+    return max(a)
 
 def main():
     
-    text=input()
-    if "I"in text:
-        n=input()
-        parents=input().split(" ")
-        parents=map(int,parents)
-        parents=list(parents)
-        print(compute_height(int(n),parents))
-    elif "F"in text:
+    text = input()
+    if "I" in text:
+        n = int(input())
+        parents = list(map(int, input().split()))
         
-        faila_nosaukums=input()
-        if "a" in file_name:
-            print("error")
+    elif "F" in text:
+
+        path = "./test/"
+        file_name = input("Enter the file name: ")
+        file_path = os.path.join(path, file_name)
+
+        if "a" not in filename:
             
-        else:
-            
-            fails=open("./test/"+file_name)
-            n=fails.readline()
-            n=int(n)
-            parents=fails.readline()
-            parents=parents.split(" ")
-            parents=map(int,parents)
-            parents=list(parents)
-            print(compute_height(n,parents))
-            fails.close()
+            try:
+                with open(file_path) as f:
+                    n = int(f.readline().strip())
+                    parents = list(map(int, f.readline().strip().split()))
+            except Exception as e:
+                print("Error:", str(e))
+                return
 
+    else:
+        print("Enter 'I' or 'F':")
+        return
 
-        pass
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
+    print(compute_height(n, parents))
 
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
