fb2cbdd5cb920bf7e633089958e09f7720f5a737
akurjans <118132555+akurjans@users.noreply.github.com>
2023-03-06 01:31:38 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,50 +1,75 @@
 import sys
 import threading
+import numpy as np
 
 def compute_height(n, parents):
-    height = [0] * n
+    # Write this function
+    x=np.array(parents)
+    
+    max_height = 0
+    max_height_ch=0
+    node_mh={}
+    # Your code here
+    k=0
+  
     for i in range(n):
-       if height[i] == 0:
-           a = 1
-           h = i
-           while parents[h] != -1:
-             h = parents[h]
-             if height[h] !=0:
-                 cur_h += height[h]
-                 break
-             a += 1
-           height[i] = a
-    return max(height)
+        k=i
+        while True:
+            if x[i]==-1:
+                #max_height=0
+                k=i
+                break
+            else:
+                max_height+=1
+                
+                i=x[i]
+                
+                if i in node_mh.keys():
+                    
+                    max_height=max_height+node_mh.get(i)
+                    node_mh[k]=max_height
+                    k=i
+                    break
+                else:
+                    node_mh[k]=max_height
+
+                 
+                
+                
+        if max_height_ch<max_height:
+            max_height_ch=max_height
+        max_height=0
+
+        
+    
+
+    return max_height_ch+1
+
 
 def main():
     
-    text = input()
-    if "I" in text:
-        n = int(input())
+    text= input("I or F:")
+    if "I" in text or "i" in text:
+        n=int(input())
         parents = list(map(int, input().split()))
+        max_augstums = compute_height(n, parents)
+        print(max_augstums)
         
-    elif "F" in text:
+    if "F" in text:
+        filename = input()
+        file='./test/'+ filename
+        if 'a' not in filename:
+            with open (file) as file:
+                n=int (file.readline())
+                parents = list(map(int, file.readline().strip().split()))
+                max_augstums = compute_height(n, parents)
+                print(max_augstums)
+        else: 
+            print ("Error: invalid filename")
 
-        path = "./test/"
-        file_name = input("Enter the file name: ")
-        file_path = os.path.join(path, file_name)
 
-        if "a" not in filename:
-            
-            try:
-                with open(file_path) as f:
-                    n = int(f.readline().strip())
-                    parents = list(map(int, f.readline().strip().split()))
-            except Exception as e:
-                print("Error:", str(e))
-                return
-
-    else:
-        print("Enter 'I' or 'F':")
-        return
-
-    print(compute_height(n, parents))
-
+        
+   
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
